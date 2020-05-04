7f035477ae15f741cbdb7816feb5d638af832ae1
Leslie <leslie.appl@gmail.com>
2020-05-04 10:24:00 +0800

    updated

--- UIScrollView50220/C/P1.swift
+++ UIScrollView50220/C/P1.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class P1: UIViewController {
-
+    
     let images = ["spot1", "spot2", "spot3", "home1", "home2", "home3", "home4", "home5"]
     var imageView: UIImageView!
     var page: Int = 0
@@ -70,6 +70,7 @@ class P1: UIViewController {
 
 //MARK: - Calculating the page
 extension P1: UIScrollViewDelegate {
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let pageWidth = mainScroll.frame.size.width
         let getPage = round(mainScroll.contentOffset.x/pageWidth)
@@ -82,7 +83,6 @@ extension P1: UIScrollViewDelegate {
 //MARK: - reset the page size when rotating the screen
 extension P1 {
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        print("rotated")
 //Catch page value before calling UIScrollViewDelegate.scrollViewDidScroll() method
 //Because, rotating the screen led to change to size of scroll view.
 //Mean while automatically to trigger the scrollViewDidScroll() method to distract the value of page property
@@ -93,18 +93,23 @@ extension P1 {
         view.layoutIfNeeded()
         switch UIDevice.current.orientation {
         case .portrait:
+            print("rotated to portrait")
             self.scrollWidth = view.frame.size.height
             self.scrollHeight = view.frame.size.width
         case .portraitUpsideDown:
+            print("rotated to portraitUpsideDown")
             self.scrollWidth = view.frame.size.height
             self.scrollHeight = view.frame.size.width
         case .landscapeLeft:
+            print("rotated to landscapeLeft")
             self.scrollWidth = view.frame.size.height
             self.scrollHeight = view.frame.size.width
         case .landscapeRight:
+            print("rotated to landscapeRight")
             self.scrollWidth = view.frame.size.height
             self.scrollHeight = view.frame.size.width
         default:
+            print("rotated to other")
             self.scrollWidth = view.frame.size.height
             self.scrollHeight = view.frame.size.width
         }
@@ -131,14 +136,19 @@ extension P1 {
             posX = posX + scrollWidth
         }
         
+            print("offSetX: \(mainScroll.contentOffset.x)")
+        
 //Scroll to page after resizing the page
         var frame = mainScroll.frame
         frame.origin.x = scrollWidth*CGFloat(page)
-        mainScroll.scrollRectToVisible(frame, animated: false)
+            print("images.count: \(self.images.count)")
+            print("scroll to page: \(page)")
+            print("mainScroll.frame.origin.x: \(frame.origin.x)")
+        
+        mainScroll.scrollRectToVisible(frame, animated: true)
         pageCounter.currentPage = page
         
-//Scroll Views may be created inside other Scroll Views
-        self.page = page
+        print("last Page: \(self.page)")
     }
     
     
