1c8245882f18b05da194709d47f9e719f430a36b
Leslie <leslie.appl@gmail.com>
2020-05-11 16:38:26 +0800

    updated

--- UIScrollView50220/C/P3.swift
+++ UIScrollView50220/C/P3.swift
@@ -24,7 +24,7 @@ class P3: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         
-        mainScroll.endEditing(true)
+        view.endEditing(true)
     }
     
     @IBAction func saveBtnPressed(_ sender: UIButton) {
